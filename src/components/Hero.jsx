fd745210e1e4d22d4039d865a6e5e31e6e13c5bd
EmanuelGCosta <emanuelgs3.abec@gmail.com>
2025-11-19 23:18:29 -0300

    refactor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
                     </h1>
                     <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
                         Unleash the power of AI within Brainwave.
-                        Upgrade your productivity witg brainwave, the
+                        Upgrade your productivity with brainwave, the
                         open AI chat app.
                     </p>
                     <Button href="#pricing" white>
